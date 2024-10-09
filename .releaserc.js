055623ef53fff10d7522cfe37bfca2d07b704934
Tomasz Wostal <tomasz@wostal.eu>
2024-10-09 22:32:34 +0200

    feat: add semantic-release basic configuration

---
+++ .releaserc.js
@@ -0,0 +1,37 @@
+const { prerelease } = require("semver");
+module.exports = {
+  branches: [
+    "main",
+    "next",
+    { name: "dev", prerelease: "alpha" },
+    { name: "qa", prerelease: "beta" }
+  ],
+  plugins: [
+    '@semantic-release/github',
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      //{
+      //  prepareCmd:
+      //    "sed -i 's/targetRevision:.*/targetRevision: v${nextRelease.version}/' app/devops-app.yaml && find applicationsets -type f -name '*.yaml' -exec sed -i 's/revision:.*/revision: v${nextRelease.version}/' {} +",
+      //},
+    ],
+    [
+      '@semantic-release/git',
+      {
+        //assets: ['CHANGELOG.md', 'app/devops-app.yaml', 'applicationsets/**/*.yaml'],
+        assets: ['CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
+};
+
---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# [0.1.0](https://github.com/devops-toys/devops-app/compare/v0.0.0...v0.1.0) (2024-10-09)
+
+
+### Features
+
+* add semantic-release basic configuration ([9bfcbba](https://github.com/devops-toys/devops-app/commit/9bfcbbac9faa4e887f864816e3db4edba34fa1d1))
